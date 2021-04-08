6209df801d3911e3f344722a914a3e277b109b52
Piotr Kołaczkowski <pkolaczk@datastax.com>
2021-04-08 19:28:16 +0200

    Enable lto and panic=abort Remove reference to unused std-semaphore crate.

--- src/semaphore.rs
+++ src/semaphore.rs
@@ -20,26 +20,6 @@ use std::sync::{Arc, Condvar, Mutex};
 /// until the counter is positive, and each release will increment the counter
 /// and unblock any threads if necessary.
 ///
-/// # Examples
-///
-/// ```
-/// use std_semaphore::Semaphore;
-///
-/// // Create a semaphore that represents 5 resources
-/// let sem = Semaphore::new(5);
-///
-/// // Acquire one of the resources
-/// sem.acquire();
-///
-/// // Acquire one of the resources for a limited period of time
-/// {
-///     let _guard = sem.access();
-///     // ...
-/// } // resources is released here
-///
-/// // Release our initially acquired resource
-/// sem.release();
-/// ```
 pub struct Semaphore {
     lock: Mutex<isize>,
     cvar: Condvar,
