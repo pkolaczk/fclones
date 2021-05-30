7bd5bcfe07c382ad4518df1230cf84bf8b82361f
Piotr Kołaczkowski <pkolaczk@datastax.com>
2021-05-30 10:22:51 +0200

    Drop file_lock crate and implement file lock directly This is needed in order to set open options that were not possible with file_lock crate.

--- src/lock.rs
+++ src/lock.rs
@@ -1,10 +1,9 @@
 use core::fmt;
 use std::fmt::{Display, Formatter};
-use std::fs::Metadata;
-use std::io;
+use std::fs::{File, Metadata};
+use std::{fs, io};
 
 use crate::path::Path;
-use std::io::ErrorKind;
 
 /// Semi-portable file locking.
 /// Provides safe access to file metadata.
@@ -13,72 +12,96 @@ use std::io::ErrorKind;
 pub struct FileLock {
     pub path: Path,
     pub metadata: Metadata,
-
-    #[cfg(unix)]
-    _lock: file_lock::FileLock,
-
-    #[cfg(windows)]
-    _lock: File,
+    pub file: File,
 }
 
 impl FileLock {
-    /// Locks a file and obtains its metadata.
-    /// On error, the error message will contain the path.
     #[cfg(unix)]
-    pub fn new(path: Path) -> io::Result<FileLock> {
-        let path_str = path.to_string_lossy();
-        let path_buf = path.to_path_buf();
-        if !path_buf.exists() {
-            return Err(io::Error::new(ErrorKind::NotFound, format!("File not found: {}", path)));
+    fn nix_as_io_error<T>(result: nix::Result<T>) -> io::Result<T> {
+        match result {
+            Ok(x) => Ok(x),
+            Err(nix::Error::Sys(errno)) => Err(io::Error::from_raw_os_error(errno as i32)),
+            Err(e) => Err(io::Error::new(io::ErrorKind::Other, e.to_string())),
         }
-        let lock = file_lock::FileLock::lock(path_str.as_str(), false, true).map_err(|e| {
-            io::Error::new(
-                e.kind(),
-                format!("Failed to acquire exclusive lock on {}: {}", path, e),
-            )
-        })?;
-        let metadata = lock.file.metadata().map_err(|e| {
-            io::Error::new(
-                e.kind(),
-                format!("Failed to read metadata of {}: {}", path, e),
-            )
-        })?;
-        Ok(FileLock {
-            _lock: lock,
-            path,
-            metadata,
-        })
+    }
+
+    #[cfg(unix)]
+    fn fcntl_lock(file: &File) -> io::Result<()> {
+        use nix::fcntl::*;
+        use std::os::unix::io::AsRawFd;
+
+        let f = libc::flock {
+            l_type: libc::F_WRLCK as i16,
+            l_whence: libc::SEEK_SET as i16,
+            l_start: 0,
+            l_len: 0,
+            l_pid: 0,
+        };
+        let result = nix::fcntl::fcntl(file.as_raw_fd(), FcntlArg::F_SETLK(&f));
+        Self::nix_as_io_error(result).map(|_| {})
+    }
+
+    #[cfg(unix)]
+    fn fcntl_unlock(file: &File) -> io::Result<()> {
+        use nix::fcntl::*;
+        use std::os::unix::io::AsRawFd;
+
+        let f = libc::flock {
+            l_type: libc::F_UNLCK as i16,
+            l_whence: libc::SEEK_SET as i16,
+            l_start: 0,
+            l_len: 0,
+            l_pid: 0,
+        };
+        let result = nix::fcntl::fcntl(file.as_raw_fd(), FcntlArg::F_SETLK(&f));
+        Self::nix_as_io_error(result).map(|_| {})
     }
 
     /// Locks a file and obtains its metadata.
     /// On error, the error message will contain the path.
-    #[cfg(windows)]
     pub fn new(path: Path) -> io::Result<FileLock> {
-        let options = fs::OpenOptions::new().read(true).write(true).create(false);
-        let file = options.open(&path.to_path_buf()).map_err(|e| {
-            io::Error::new(e.kind(), format!("Failed to open file {}: {}", path, e))
-        })?;
+        let path_buf = path.to_path_buf();
+        let file = fs::OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(false)
+            .open(&path_buf)
+            .map_err(|e| {
+                io::Error::new(e.kind(), format!("Failed to open file {}: {}", path, e))
+            })?;
+
+        #[cfg(unix)]
+        if let Err(e) = Self::fcntl_lock(&file) {
+            return Err(io::Error::new(
+                e.kind(),
+                format!("Failed to lock file {}: {}", path, e),
+            ));
+        };
+
         let metadata = file.metadata().map_err(|e| {
             io::Error::new(
                 e.kind(),
                 format!("Failed to read metadata of {}: {}", path, e),
             )
         })?;
+
         Ok(FileLock {
-            _lock: file,
             path,
             metadata,
+            file,
         })
-    }
-
-    /// Releases the lock and returns the file path and metadata
-    pub fn release(self) -> (Path, Metadata) {
-        (self.path, self.metadata)
     }
 }
 
 impl Display for FileLock {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         f.pad(format!("{}", self.path).as_str())
+    }
+}
+
+impl Drop for FileLock {
+    fn drop(&mut self) {
+        #[cfg(unix)]
+        let _ = Self::fcntl_unlock(&self.file);
     }
 }
