dc7597677662449e37eb8992bc861b17f7b234a3
UP2GeeK <{ID}+{username}@users.noreply.github.com>
2022-04-22 12:19:50 -0500

    Update memory query to new sysinfo function format
    Workaround for sysinfo build failures on trueNAS freebsd 12 system

--- src/device.rs
+++ src/device.rs
@@ -96,7 +96,6 @@ impl DiskDevice {
         FileLen(match self.disk_type {
             DiskType::SSD => 4 * 1024,
             DiskType::HDD => 4 * 1024,
-            DiskType::Removable => 4 * 1024,
             DiskType::Unknown(_) => 4 * 1024,
         })
     }
@@ -105,7 +104,6 @@ impl DiskDevice {
         FileLen(match self.disk_type {
             DiskType::SSD => 4 * 1024,
             DiskType::HDD => 16 * 1024,
-            DiskType::Removable => 16 * 1024,
             DiskType::Unknown(_) => 16 * 1024,
         })
     }
@@ -118,7 +116,6 @@ impl DiskDevice {
         FileLen(match self.disk_type {
             DiskType::HDD => 64 * 1024 * 1024, // 64 MB
             DiskType::SSD => 64 * 1024,        // 64 kB
-            DiskType::Removable => 64 * 1024 * 1024,
             DiskType::Unknown(_) => 64 * 1024 * 1024,
         })
     }
@@ -168,7 +165,6 @@ impl DiskDevices {
                 let p = match disk_type {
                     DiskType::SSD => pool_sizes.get(OsStr::new("ssd")),
                     DiskType::HDD => pool_sizes.get(OsStr::new("hdd")),
-                    DiskType::Removable => pool_sizes.get(OsStr::new("removable")),
                     DiskType::Unknown(_) => pool_sizes.get(OsStr::new("unknown")),
                 };
                 match p {
@@ -244,17 +240,17 @@ impl DiskDevices {
             String::from("unknown"),
             pool_sizes,
         );
-        for d in sys.get_disks() {
-            let device_name = Self::physical_device_name(d.get_name());
+        for d in sys.disks() {
+            let device_name = Self::physical_device_name(d.name());
             let index = result.add_device(
                 device_name,
-                d.get_type(),
-                String::from_utf8_lossy(d.get_file_system()).to_string(),
+                d.type_(),
+                String::from_utf8_lossy(d.file_system()).to_string(),
                 pool_sizes,
             );
             result
                 .mount_points
-                .push((Path::from(d.get_mount_point()), index));
+                .push((Path::from(d.mount_point()), index));
         }
         result
             .mount_points
