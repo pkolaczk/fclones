c6e90226867fd0ec8a446bc677cd003d8c64a734
Piotr Kołaczkowski <pkolaczk@datastax.com>
2023-06-12 09:58:18 +0200

    Update packaging scripts

--- packaging/package-internal.sh
+++ packaging/package-internal.sh
@@ -10,20 +10,20 @@ cd "$(dirname $0)/.."
 
 echo "${bold}Building${normal}"
 set -x
-cargo build --release
+cargo build -p fclones -p fclones-gui --release
 cargo build --release --target i686-unknown-linux-musl
 cargo build --release --target x86_64-unknown-linux-musl
 cargo build --release --target x86_64-pc-windows-gnu
 set +x
 
-echo "${bold}Packaging${normal}"
+echo "${bold}Packaging${normal} fclones"
 set -x
-VERSION=$(cargo pkgid | sed 's/.*#//')
-PKG_DIR=target/packages/$VERSION
+VERSION=$(cargo pkgid -p fclones | sed 's/.*#//')
+PKG_DIR=target/packages/fclones-$VERSION
 mkdir -p $PKG_DIR
 rm -f $PKG_DIR/*
 
-cargo deb
+cargo deb -p fclones
 mv target/debian/*.deb $PKG_DIR
 
 fakeroot alien --to-rpm -c $PKG_DIR/*.deb
@@ -34,3 +34,18 @@ mv *.tgz $PKG_DIR/"fclones-$VERSION-linux-glibc-x86_64.tar.gz"
 tar -zcvf $PKG_DIR/"fclones-$VERSION-linux-musl-x86_64.tar.gz" target/x86_64-unknown-linux-musl/release/fclones
 tar -zcvf $PKG_DIR/"fclones-$VERSION-linux-musl-i686.tar.gz" target/i686-unknown-linux-musl/release/fclones
 zip -j $PKG_DIR/"fclones-$VERSION-windows-x86_64.zip" target/x86_64-pc-windows-gnu/release/fclones.exe
+
+echo "${bold}Packaging${normal} fclones-gui"
+set -x
+VERSION=$(cargo pkgid -p fclones-gui | sed 's/.*#//')
+PKG_DIR=target/packages/fclones-gui-$VERSION
+mkdir -p $PKG_DIR
+rm -f $PKG_DIR/*
+
+cargo deb -p fclones-gui
+mv target/debian/*.deb $PKG_DIR
+
+fakeroot alien --to-rpm -c $PKG_DIR/*.deb
+mv *.rpm $PKG_DIR
+fakeroot alien --to-tgz -c $PKG_DIR/*.deb
+mv *.tgz $PKG_DIR/"fclones-gui-$VERSION-linux-glibc-x86_64.tar.gz"
